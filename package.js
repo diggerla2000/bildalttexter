dac27631725acb9e7b27e45240a6f644333703f2
gawelskitools <sascha.gawel@gmx.de>
2024-10-06 00:26:12 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "my-proxy-server",
+  "version": "1.0.0",
+  "description": "Backend-Proxy-Server für GitHub Actions",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "node-fetch": "^2.6.1",
+    "dotenv": "^10.0.0"
+  },
+  "engines": {
+    "node": "14.x"
+  },
+  "license": "MIT"
+}
